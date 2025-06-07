89c04d5e79378e75964d98ae537566468009a89e
its-anya <anya.suzme@gmail.com>
2025-06-07 12:35:03 +0530

    v3

--- src/services/streamingApi.js
+++ src/services/streamingApi.js
@@ -54,5 +54,56 @@ export const STREAMING_PROVIDERS = [
     getMovieUrl: (id) => `https://multiembed.mov/directstream.php?video_id=${id}`,
     getTvUrl: (id, season, episode) => 
       `https://multiembed.mov/directstream.php?video_id=${id}&s=${season}&e=${episode}`
+  },
+  // MovieDex Providers
+  {
+    id: 'vidzee',
+    name: 'Vidzee (MovieDex)',
+    getMovieUrl: (id) => `https://vidzee.streamapp.site/embed/movie/${id}`,
+    getTvUrl: (id, season, episode) => 
+      `https://vidzee.streamapp.site/embed/tv/${id}/${season}/${episode}`
+  },
+  {
+    id: 'autoembed',
+    name: 'AutoEmbed (MovieDex)',
+    getMovieUrl: (id) => `https://autoembed.co/movie/tmdb/${id}`,
+    getTvUrl: (id, season, episode) => 
+      `https://autoembed.co/tv/tmdb/${id}/${season}/${episode}`
+  },
+  {
+    id: 'xprime',
+    name: 'XPrime (MovieDex)',
+    getMovieUrl: (id) => `https://xprime.app/embed/movie/${id}`,
+    getTvUrl: (id, season, episode) => 
+      `https://xprime.app/embed/tv/${id}/${season}/${episode}`
+  },
+  {
+    id: 'vidsrcsu',
+    name: 'VidSrc.su (MovieDex)',
+    getMovieUrl: (id) => `https://vidsrc.su/embed/movie/${id}`,
+    getTvUrl: (id, season, episode) => 
+      `https://vidsrc.su/embed/tv/${id}/${season}/${episode}`
+  },
+  {
+    id: 'embedsu',
+    name: 'Embed.su (MovieDex)',
+    getMovieUrl: (id) => `https://embed.su/embed/movie/${id}`,
+    getTvUrl: (id, season, episode) => 
+      `https://embed.su/embed/tv/${id}/${season}/${episode}`
+  },
+  {
+    id: 'vidsrcvip',
+    name: 'VidSrc.vip (MovieDex)',
+    getMovieUrl: (id) => `https://vidsrc.vip/embed/movie/${id}`,
+    getTvUrl: (id, season, episode) => 
+      `https://vidsrc.vip/embed/tv/${id}/${season}/${episode}`
+  },
+  // Anime Provider
+  {
+    id: 'hianime',
+    name: 'HiAnime (MovieDex)',
+    getMovieUrl: (id) => `https://hianime.pstream.org/player/${id}`,
+    getTvUrl: (id, season, episode) => 
+      `https://hianime.pstream.org/player/${id}/${episode}`
   }
 ];
